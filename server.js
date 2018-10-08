2de60fd53347c5bf9f860355c08cb031963d5ed4
Your Name <poopdingger@gmail.com>
2018-10-08 15:21:50 -0400

    lab 6 stuff

--- server.js
+++
@@ -1,8 +0,0 @@
-//const express = require('express')
-
-console.log("stuff")
-let num = [1,3,2,3,22]
-console.log(num.sort((a,b)=>a>b?1:b>a?-1:0))
-
-let evens = num.filter((a)=>a%2==0)
-console.log(evens)
