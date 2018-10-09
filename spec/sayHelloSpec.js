845ea0bb46902ff0579eb40a6685bc180ef5d079
Your Name <poopdingger@gmail.com>
2018-10-09 08:51:06 -0400

    sayHello

--- spec/sayHelloSpec.js
+++ spec/sayHelloSpec.js
@@ -1,9 +1,56 @@
 var sayHello = require("../src/sayHello.js");
 
 describe("Greet", function (){
+  //hello name
   it("concats Hello and a name", function(){
-    var actual = sayHello.greet("World");
+    var sayHello1  = new sayHello();
+    var actual = sayHello1.greet("World");
     var expected = "Hello, World";
     expect(actual).toEqual(expected);
   });
+
+  //hello day time
+  it("time of day hello", function(){
+    var sayHello1   = new sayHello();
+    var actual = sayHello1.greet("Good morning");
+    var expected = "Good morning";
+    expect(actual).toEqual(expected);
+
+    var actual = sayHello1.greet("Good afternoon");
+    var expected = "Good afternoon";
+    expect(actual).toEqual(expected);
+
+    var actual = sayHello1.greet("Good evening");
+    var expected = "Good evening";
+    expect(actual).toEqual(expected);
+  });
+
+  //hello empty
+  it("Login prompt for empty", function(){
+    var sayHello2 =  new sayHello();
+    var actual = sayHello2.greet("");
+    var expected = "Please Login";
+    expect(actual).toEqual(expected);
+  });
+
+
+    //hello German
+    it("replies Hallo to Hallo", function(){
+      var sayHello3 =   new sayHello();
+      var actual = sayHello3.greet("Hallo");
+      var expected = "Hallo";
+      expect(actual).toEqual(expected);
+    });
+
+    //hello x4
+    it("4x hello", function(){
+      var sayHello4 = new sayHello();
+      sayHello4.greet("Hallo");
+      sayHello4.greet("Hallo");
+      sayHello4.greet("Hallo");
+      sayHello4.greet("Hallo");
+      var actual = sayHello4.greet("Hallo");
+      var expected = "";
+      expect(actual).toEqual(expected);
+    });
 });
--- src/sayHello.js
+++ src/sayHello.js
@@ -1,3 +1,33 @@
-exports.greet = function greet(name){
-  return "Hello, "+name;
+
+var sayHello = function(){
+  this.count =0;
+  return this;
+}
+
+sayHello.prototype.greet = function(name){
+  this.count=this.count+1;
+  if (this.count>4){
+    return "";
+  }
+  switch(name){
+    case "Good morning":
+      return "Good morning";
+      break;
+    case "Good afternoon":
+      return "Good afternoon";
+      break;
+    case "Good evening":
+      return "Good evening";
+      break;
+    case "Hallo":
+      return "Hallo";
+      break;
+    case "":
+      return "Please Login";
+      break;
+    default:
+      return "Hello, "+name;
+  }
+
 };
+module.exports = sayHello;
